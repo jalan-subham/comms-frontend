0c8eea39240449f6806a6e0e11fc26f6f80f1d74
Subham Jalan <subham.jalan@plaksha.edu.in>
2024-07-27 15:42:04 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E40AF",
+        secondary: "#F87171",
+        tertiary: "#FBBF24",
+        dark: "#1F2937",
+        light: "#F9FAFB",
+      },
+    },
+  },
+  plugins: [],
+} 
